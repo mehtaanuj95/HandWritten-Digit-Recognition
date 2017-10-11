f30d5a69d85f99e09a6eb9cb26fce70a2e69acb6
Anuj Bhai Mehta <mehta.anuj2309@gmail.com>
2017-10-11 23:48:27 +0530

    added weights

--- script.m
+++ script.m
@@ -13,3 +13,14 @@ m = size(X, 1);
 rand_indices = randperm(m); 			% Randomly select 100 data points to display
 selected = X(rand_indices(1:100), :);
 displayData(selected);
+
+%% Load weights
+
+load('weights.mat');
+
+%% Prediction of digit classes
+
+pred = predict(Theta1, Theta2, X);
+fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
+
+pause;
